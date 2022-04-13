0e937b1dff8fe2599e1c1ce786a410638e7e4e85
Tatsiana-Savashchuk <nodlesh@rambler.ru>
2022-04-13 13:52:44 +0300

    fixed small issues

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import './index.css';
-import App from './App';
+import { App } from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
