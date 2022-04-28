feb1aee490b3785ae8f3a30ba1ab408b03096a0b
Tatsiana-Savashchuk <nodlesh@rambler.ru>
2022-04-28 12:59:28 +0300

    improved header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,14 +1,37 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import './Header.css';
-import { MENU_ITEMS_ARRAY } from '../../constants/common.js';
+import { uniqueId } from 'lodash';
+
+const handleLogout = () => {
+  localStorage.removeItem('active');
+};
 
 export const Header = () => {
+  const location = useLocation();
+
+  const menuItemsArray = [
+    { title: 'Home', url: "/" }, 
+    { title: 'Appointment', url: "/appointment" }, 
+    { title: 'Price', url: "/price" },
+    { title: 'Contact', url: "/contact" }, 
+    {
+      title: localStorage.getItem('active')
+      ? <i className="bi bi-suit-heart-fill sign-out" onClick={handleLogout}></i> 
+      : <i className="bi bi-suit-heart sign-in"></i>
+        
+      , 
+      url: localStorage.getItem('active') ? location.pathname : "/login",
+    },
+  ];
+
   return (
-    <header>
+    <header className="header">
       <h1>SAVA</h1>
       <nav>
         <ul>
-          {MENU_ITEMS_ARRAY.map(({ title, url }) => <li><Link to={url} className="menu-item uppercase">{title}</Link></li>)}
+          {menuItemsArray.map(({ title, url }) => 
+            <li key={uniqueId('menu_item_')}><Link to={url} className="menu-item uppercase">{title}</Link></li>
+          )}
         </ul>
       </nav>
     </header>
--- src/constants/common.js
+++ src/constants/common.js
@@ -1,15 +1,1 @@
-export const MENU_ITEMS_ARRAY = [
-  { title: 'Home', url: "/" }, 
-  { title: 'Appointment', url: "/appointment" }, 
-  { title: 'Price', url: "/price" }, 
-  { title: 'Contact', url: "/contact" }, 
-  {
-    title:
-      <> 
-        <i className="bi bi-suit-heart"></i>
-        <i className="bi bi-suit-heart-fill" hidden></i>
-      </>
-    , 
-    url: "/login",
-  },
-];
+export const PREFIX = 'https://it-academy-js-api-zmicerboksha.vercel.app/api/st/';
