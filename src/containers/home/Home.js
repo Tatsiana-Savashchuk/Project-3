f292e8bf614b9a85506804f686fc9b420204df73
Tatsiana-Savashchuk <nodlesh@rambler.ru>
2022-06-02 17:44:18 +0300

    added sass dependency & added home page

--- src/containers/home/Home.js
+++ src/containers/home/Home.js
@@ -1,5 +1,23 @@
 import { PageBuilder } from '../../components/pageBuilder/PageBuilder.js';
+import { Button } from '../../components/button/Button.js';
+import { useNavigate } from 'react-router-dom';
+import './Home.scss';
+
+const HomeContent = () => {
+  const navigate = useNavigate();
+  const goToAppointment = () => {
+    navigate('/appointment');
+  }
+
+  return (
+    <div className='home-content'>
+      <p className='home-content-header'>manicure studio <br /><span className="uppercase">SAVA</span></p>
+      <p className='home-content-description'>we take care of every client's nails</p>
+      <Button text='to make an appointment' onClickFuction={goToAppointment} uppercase />
+    </div>
+  )
+}
 
 export const Home = () => {
-  return <PageBuilder />;
+  return <PageBuilder main={<HomeContent />} />;
 };
